c145e664a2383e6181f6880462fbf256f2eed1fa
MoolaSmoola <byg1@williams.edu>
2024-11-04 11:09:56 -0500

    doing the ols

--- econ370dhs.R
+++ econ370dhs.R
@@ -506,14 +506,270 @@ mse_comparison <- tibble(Method = names(collected_variables_names),
 lasso_dd_frequency <- frequency_vars %>%
   filter(collected_variables %in% lasso_dd_vars)
 
+# or cross-validate the non-regularization techniques ( which would be insane )
 
 
-# or cross-validate the non-regularization techniques: 
+mypath <- "~/ECON370/ECON370dhs/econ370 project/"
+
+ethiopia.dhs <- read_dta(paste0(mypath, "Ethiopia2016/ETBR71FL.dta")) %>% 
+  filter(!is.na(midx)) %>% 
+  filter(b5 == 1) %>% # Child is alive
+  filter(v135 == 1) # Usual resident or visitor of Ethiopia
 
 
 
+ethiopia.data <- ethiopia.dhs %>% 
+  select(hw70, hw1, bord, b0, b1, b2, b4, b11, 
+         v012, v024, v025, v113, v116, v119, v133,
+         v136, v151, v160, v161, v190, v212, v745a,
+         v745b, v367, m15, v152, v153, v213, v218,
+         v404, v409, v409a, v410, v411, v411a,
+         v412a, v412c, v414e, v414f, v414g, v414h,
+         v414i, v414j, v414k, v414l, v414m, v414n,
+         v414o, v414p, v416, v504, v505, v506, v508,v511,
+         v605, v613, v624, v704, v714, v715, v716, v719, v730,
+         v743a, v743b, v743f, v744a, v744b, v744c,
+         v744d, v744e, v746, m4, m18, m19, m45, h2,
+         h3, h4, v120, v121, v122, v124, v125,
+         v131, v157, v158, ml101, v201, v206, v207,
+         v384a, v384b, v384c, v393, v394, v501, v503, v621,
+         v627, v628) %>%   # ~ 100 at the moment
+  rename(haz = hw70, 
+         age_months = hw1, 
+         mob = b1, 
+         yob = b2, 
+         sex = b4, 
+         interval = b11, 
+         mom_age = v012,
+         partner_age = v730,
+         region = v024,
+         urban_rural = v025,
+         water_source = v113,
+         sanitation = v116,
+         electricity = v119,
+         radio = v120,
+         television = v121,
+         refrigerator = v122, 
+         motorcycle = v124,
+         car_truck = v125,
+         ethnicity = v131,
+         mothers_education = v133,
+         household_size = v136,
+         household_head_sex = v151, # we should group the variables for the slides
+         household_head_age = v152,
+         telephone = v153,
+         reading_newspaper = v157,
+         listening_radio = v158,
+         mosquito_net = ml101,
+         total_children = v201,
+         son_died = v206,
+         daughter_died = v207,
+         familyplanning_radio = v384a,
+         familyplanning_tv = v384b,
+         familyplanning_newsp = v384c,
+         familyplanning_visit = v393,
+         healthfacility_visit = v394,
+         toilet_shared = v160,
+         fuel = v161,
+         wealth_index = v190,
+         age_first_birth = v212,
+         currently_pregnant = v213,
+         number_living_children = v218,
+         currently_breastfeeding = v404,
+         plain_water = v409, # nutrition related variables
+         sugar_water = v409a,
+         juice = v410,
+         milk = v411, 
+         formula = v411a,
+         fortified_food = v412a,
+         soup_broth = v412c,
+         grain_foods = v414e,
+         tuber_foods = v414f,
+         eggs = v414g,
+         meat = v414h,
+         squash = v414i,
+         leafy_vegetables = v414j,
+         vitamin_a_fruits = v414k,
+         other_fruits = v414l,
+         organ_meat = v414m,
+         fish = v414n,
+         legumes = v414o,
+         dairy = v414p,
+         oral_hydration = v416,  # knowledge of medical practices
+         current_marital = v501,
+         number_unions = v503,
+         living_with_partner = v504, # household arrangements (attention theory)
+         other_wives = v505,
+         wife_rank = v506,
+         yo_first_cohabitation = v508,
+         age_first_cohabitation = v511,
+         desire_more_children = v605,
+         ideal_num_children = v613,
+         husband_desire_children = v621,
+         unmet_contraceptive_need = v624,  # family planning/knowledge go together
+         ideal_num_boys = v627,
+         ideal_num_girls = v628,
+         husbands_vocation = v704,
+         working = v714, # figure out how to do years of education for the father
+         husband_education = v715,
+         vocation = v716,
+         employer = v719, # fits into the Profs. Jakiela & Ozier paper, self-employment happier
+         healthcare_decisionmaker = v743a,
+         purchase_decisionmaker = v743b, # big purchases
+         money_decisionmaker = v743f,
+         beating_leaving = v744a, 
+         beating_neglect = v744b, # this seems related to child development in two ways
+         beating_argument = v744c,
+         beating_refusesex = v744d,
+         beating_burnedfood = v744e,
+         biggestearner = v746, # plausibly if the woman makes more..
+         breastfeeding_duration = m4,
+         birth_size = m18,
+         birth_weight = m19,
+         iron_supplement = m45,
+         tuberculosis_vaccine = h2,
+         dtap_vaccine = h3,
+         polio_vaccine = h4,
+         homeowner = v745a,
+         landowner = v745b,
+         child_wanted = v367,
+         pob = m15
+  )
+
+## drop observations with missing height-for-age z-scores
+ethiopia.data$haz <- as.numeric(ethiopia.data$haz)
+ethiopia.data <- filter(ethiopia.data, !is.na(haz) & haz!=9996 & haz!=9997 & haz!= 9998)
+
+## idiosyncratic cleaning
+
+## change age at time of survey to age at birth
+ethiopia.data$mom_age <- ethiopia.data$mom_age - (2014 - ethiopia.data$yob)
+
+## twin dummy
+ethiopia.data$twin = ifelse(ethiopia.data$b0 == 0, 0, 1)
+ethiopia.data  <- select(ethiopia.data, !b0)
+
+## birth interval cannot be NAN (first births), replace with median
+ethiopia.data$interval[is.na(ethiopia.data$interval)] <- median(ethiopia.data$interval, na.rm=TRUE)
+
+# make dummies for factor variables:
+factor_cols_ethiopia <- c("mob",
+                 "yob", 
+                 "sex",
+                 "bord",
+                 "region",
+                 "urban_rural",
+                 "water_source",
+                 "sanitation",
+                 "electricity",
+                 "radio",
+                 "television",
+                 "refrigerator",
+                 "motorcycle",
+                 "car_truck",
+                 "ethnicity",
+                 "telephone",
+                 "reading_newspaper",
+                 "listening_radio",
+                 "household_head_sex",
+                 "toilet_shared",
+                 "fuel",
+                 "wealth_index",
+                 "homeowner",
+                 "landowner",
+                 "child_wanted",
+                 "pob",
+                 "twin",
+                 "mosquito_net",
+                 "familyplanning_radio",
+                 "familyplanning_tv",
+                 "familyplanning_newsp",
+                 "familyplanning_visit",
+                 "healthfacility_visit",
+                 "currently_pregnant",
+                 "currently_breastfeeding",
+                 "plain_water",
+                 "sugar_water",
+                 "juice",
+                 "milk",
+                 "formula",
+                 "fortified_food",
+                 "soup_broth",
+                 "grain_foods",
+                 "tuber_foods",
+                 "eggs",
+                 "meat",
+                 "squash",
+                 "leafy_vegetables",
+                 "vitamin_a_fruits",
+                 "other_fruits",
+                 "organ_meat",
+                 "fish",
+                 "legumes",
+                 "dairy",
+                 "oral_hydration",
+                 "current_marital",
+                 "living_with_partner",
+                 "yo_first_cohabitation",
+                 "husband_desire_children",
+                 "unmet_contraceptive_need",
+                 "husbands_vocation",
+                 "working",
+                 "vocation",
+                 "employer",
+                 "healthcare_decisionmaker",
+                 "purchase_decisionmaker",
+                 "money_decisionmaker",
+                 "beating_leaving",
+                 "beating_neglect",
+                 "beating_argument",
+                 "beating_refusesex",
+                 "beating_burnedfood",
+                 "biggestearner",
+                 "breastfeeding_duration",
+                 "birth_size",
+                 "iron_supplement",
+                 "tuberculosis_vaccine",
+                 "dtap_vaccine",
+                 "polio_vaccine"
+)
+
+ethiopia.data[factor_cols_ethiopia] <- lapply(ethiopia.data[factor_cols_ethiopia], as.character)
+ethiopia.data <- dummy_cols(ethiopia.data, 
+                            select_columns = factor_cols_ethiopia, 
+                            remove_selected_columns = TRUE)
+ethiopia.data[is.na(ethiopia.data)] <- 0
+
+
+##### PREPARE DATA --------------------------------------
+
+Y.ethiopia = ethiopia.data$haz
+
+X.ethiopia <- ethiopia.data %>%
+  select(!c(haz)) %>% ### select the variables selected by the lasso-data.driven
+  select(any_of(lasso_dd_vars)) %>% #### but... what do we do when regions aren't the same!
+  as.matrix() # still need to clean this heavily
 
 
 
+ols.ethiopia <- lm(Y.ethiopia ~ X.ethiopia)
+summary.ethiopia <- summary(ols.ethiopia)
+p_values.ethiopia <- coef(summary.ethiopia)[, "Pr(>|t|)"]
+p_value_95.ethiopia <- p_values.ethiopia <= 0.05 
+significant_var.ethiopia <- rownames(coef(summary.ethiopia))[p_value_95.ethiopia] %>%
+  print() ## which variables are statistically significant predictors of literacy (95% level)?
+coefficients.ethiopia <- coef(summary.ethiopia)[, "Estimate"]
+min_p_value.ethiopia <- which.min(p_values.ethiopia)
+lowest_p_value.ethiopia <- p_values.ethiopia[min_p_value.ethiopia]
+lowest_p_value_var.ethiopia <- rownames(coef(summary.ethiopia))[min_p_value.ethiopia] %>%
+  print() ## which variable has the lowest p-value? 
+lowest_p_value_coeff.ethiopia <- coefficients.ethiopia[min_p_value.ethiopia] %>% 
+  print() ## what is the OLS coefficient associated with that variable? 
 
+ols_df.ethiopia <- ols.ethiopia %>%
+  tidy() %>%
+  as.data.frame()
 
+ethiopia.predictions <- predict(ols.ethiopia)
+ethiopia.ols.mse <- mean((Y.ethiopia - ethiopia.predictions)^2)
+ethiopia.ols.mse
